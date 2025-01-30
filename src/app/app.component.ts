046e23beb84a791f06fc9b26f9c4de1504c8a0d6
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2025-01-31 00:22:46 +0100

    render customer list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 import { CustomerComponent } from './components/customer/customer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CustomerComponent],
+  imports: [CustomerComponent],
   template: '<app-customer></app-customer>',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-}
+export class AppComponent {}
--- src/app/components/customer/customer.component.ts
+++ src/app/components/customer/customer.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule, DatePipe} from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgbPaginationModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CustomerService } from '../../services/customer.service';
 
 @Component({
   selector: 'app-customer',
-  imports: [],
+  imports: [CommonModule, FormsModule, NgbModule, NgbPaginationModule],
+  providers: [DatePipe],
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.scss'
 })
-export class CustomerComponent {
+export class CustomerComponent implements OnInit {
+  private customerService = inject(CustomerService);
+  private datePipe = inject(DatePipe)
+
+  currentPage = 1;
+  itemsPerPage = 5;
+  itemsPerPageOptions = [5, 10, 15, 20];
+  searchText = '';
+
+  customers = <any[]>[];
+  totalPages = 1;
+  totalCustomers = 0;
+
+  ngOnInit() {
+    this.fetchCustomers();
+  }
+
+  fetchCustomers() {
+    this.customerService
+      .getCustomers(this.currentPage, this.itemsPerPage, this.searchText)
+      .subscribe(response => {
+        const data = response.data;
+
+        this.customers = data.data;
+        this.totalPages = data.last_page;
+        this.totalCustomers = data.total;
+      });
+  }
+
+  onPageChange(page: number) {
+    this.currentPage = page;
+    this.fetchCustomers();
+  }
+
+  onItemsPerPageChange(event: any) {
+    this.itemsPerPage = event.target.value;
+    this.fetchCustomers();
+  }
+
+  formatDate(date: string) {
+    return this.datePipe.transform(date, 'MMM dd, yyyy, h:mm:ss a') + ' GMT';
+  }
 
 }
