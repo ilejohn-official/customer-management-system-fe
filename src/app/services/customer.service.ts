9ab73d77766ef286e404716af71204b0b65ce609
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2025-01-30 18:17:31 +0100

    set up service class to connect to api

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,9 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  private apiUrl = `${environment.apiUrl}/customers`;
+  private http = inject(HttpClient);
+
+  /**
+   * Get customers with pagination and search
+   */
+  getCustomers(page: number, pageSize: number, searchText: string = ''): Observable<any> {
+    const params = new HttpParams()
+      .set('page', page.toString())
+      .set('page_size', pageSize.toString())
+      .set('search_text', searchText);
+
+    return this.http.get<any>(this.apiUrl, { params });
+  }
+
+  /**
+   * Create a new customer
+   */
+  createCustomer(customer: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, customer);
+  }
+
+  /**
+   * Update an existing customer
+   */
+  updateCustomer(id: number, customer: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/${id}`, customer);
+  }
 }
