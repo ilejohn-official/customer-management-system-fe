2c7f2e09e0d76cfc76037fde8954a05cad28dfed
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2025-01-31 06:50:21 +0100

    handle errors | add editing function

--- src/app/components/customer-form/customer-form.component.ts
+++ src/app/components/customer-form/customer-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
@@ -11,11 +11,16 @@ import { ReplaceUnderscorePipe } from '../../pipes/replace-underscore.pipe';
   templateUrl: './customer-form.component.html',
   styleUrl: './customer-form.component.scss'
 })
-export class CustomerFormComponent {
+export class CustomerFormComponent implements OnInit {
   private customerService = inject(CustomerService);
   private fb = inject(FormBuilder);
-  customerForm: FormGroup;
   private activeModal = inject(NgbActiveModal);
+
+  customerForm: FormGroup;
+  controlNames: string[] = [];
+  @Input() selectedCustomer: any = null;
+  isEditing: boolean = false;
+  formErrors: string[] = [];
 
   constructor() {
     this.customerForm = this.fb.group({
@@ -36,6 +41,15 @@ export class CustomerFormComponent {
       voters_card: [null],
       drivers_licence: [null],
     });
+
+    this.controlNames = Object.keys(this.customerForm.controls).filter(controlName => controlName !== 'company_id');
+  }
+
+  ngOnInit() {
+    if (this.selectedCustomer) {
+      this.isEditing = true;
+      this.customerForm.patchValue(this.selectedCustomer);
+    }
   }
 
   saveCustomer() {
@@ -45,10 +59,48 @@ export class CustomerFormComponent {
       return;
     }
 
-    this.customerService.createCustomer(this.customerForm.value).subscribe((response) => {
-      console.log('Customer created:', response);
-      this.activeModal.close();
-    });
+    if (this.isEditing) {
+      // Update existing customer
+      this.customerService.updateCustomer(this.selectedCustomer.id, this.customerForm.value).subscribe(
+      {
+        next: (response) => {
+            console.log('Customer updated:', response);
+            this.activeModal.close(response);
+        },
+       error: (err) => {
+          this.handleErrors(err)
+       }
+      });
+    } else {
+      // Create new customer
+      this.customerService.createCustomer(this.customerForm.value).subscribe({
+        next: (response) => {
+          console.log('Customer created:', response);
+          this.activeModal.close(response);
+        },
+        error: (err) => {
+          this.handleErrors(err)
+        }
+      })
+    }
+
+  }
+
+  handleErrors(response: any) {
+    this.formErrors = [];
+    console.log(response.error.errors);
+
+    let errors = response?.error?.errors;
+    if(errors instanceof Object && Object.keys(errors).length > 0){
+      Object.keys(errors).forEach((controlName) => {
+        const control = this.customerForm.get(controlName);
+        if (control) {
+          this.formErrors.push(`${controlName} : ${errors[controlName].join(', ')}`)
+        }
+      })
+    } else {
+      this.formErrors.push(response?.error?.message)
+    }
   }
 
   close() {
